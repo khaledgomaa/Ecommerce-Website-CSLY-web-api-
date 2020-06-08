c0c532e57b5bae12b065dce2823c9cbf34b74423
KhaledGomaa <KhaledGomaa670@gmail.com>
2020-06-08 08:46:24 +0200

    updateJwtAuthorizeToBeMoreClean

--- CSLY/MyAuthorizations/JwtAuthorize.cs
+++ CSLY/MyAuthorizations/JwtAuthorize.cs
@@ -2,14 +2,11 @@
 using System.Web;
 using CSLY.Controllers.api;
 using System.Linq;
-using System.Security.Principal;
 using Microsoft.AspNet.Identity;
 using CSLY.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Web.Http;
 using System.Web.Http.Controllers;
-using System.Web.UI;
-using System.Net;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using Newtonsoft.Json;
@@ -19,12 +16,21 @@ namespace CSLY.MyAuthorizations
 
     public class JwtAuthorize : AuthorizeAttribute 
     {
+       
+
         protected override bool IsAuthorized(HttpActionContext httpContext)
         {
+
             JwtController jwtactions = new JwtController();
+
+             var UserManager = new UserManager<ApplicationUser>
+                                (new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
             var request = httpContext.Request;
             var headers = request.Headers;
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            bool isUserAuthorized = false;
+
             if (headers.Contains("Authorization"))
             {
                 string token = headers.GetValues("Authorization").FirstOrDefault();
@@ -32,31 +38,30 @@ namespace CSLY.MyAuthorizations
                 {
                     string tokenUsername = jwtactions.ValidateToken(token);
 
-                    if (tokenUsername == null)
-                        throw new HttpResponseException(HttpStatusCode.Unauthorized);
-                    else if (UserManager.FindByName(tokenUsername) == null)
-                    {
-                        throw new HttpResponseException(HttpStatusCode.Unauthorized);
-                    }
-                    else
-                    {
-                        var userRole = UserManager.GetRoles(UserManager.FindByName(tokenUsername).Id);
-                        var path = HttpContext.Current.Server.MapPath("/loginRole.json");
-                        JObject userData = new JObject(
-                            new JProperty("Role", userRole));
+                    isUserAuthorized =
+                         (tokenUsername == null || UserManager.FindByName(tokenUsername) == null) ? false : true;
 
-                        using (StreamWriter file = File.CreateText(path))
-                        using (JsonTextWriter writer = new JsonTextWriter(file))
-                        {
-                            userData.WriteTo(writer);
-                        }
-
-                        return true;
+                    if (isUserAuthorized)
+                    {
+                        WriteUserRoleToFile(UserManager
+                            .GetRoles(UserManager.FindByName(tokenUsername).Id));
                     }
                 }
-                return false;
             }
-            return false;
+            return isUserAuthorized;
+        }
+
+        private void WriteUserRoleToFile(dynamic userRole)
+        {
+            var path = HttpContext.Current.Server.MapPath("/loginRole.json");
+            JObject userData = new JObject(
+                new JProperty("Role", userRole));
+
+            using (StreamWriter file = File.CreateText(path))
+            using (JsonTextWriter writer = new JsonTextWriter(file))
+            {
+                userData.WriteTo(writer);
+            }
         }
     }
 }
